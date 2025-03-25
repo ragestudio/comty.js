b6280b59dfa2c688383c7dc3208af5cd9b953c50
SrGooglo <srgooglo@ragestudio.net>
2025-03-25 22:54:44 +0000

    support search with addons

--- src/models/search/index.js
+++ src/models/search/index.js
@@ -1,59 +1,58 @@
 import request from "../../request"
+import processAddons from "../../helpers/processWithAddons"
+import standartListMerge from "../../utils/standartListMerge"
 
 export default class Search {
-    /**
-     * Performs a search using the provided keywords and optional parameters.
-     *
-     * @param {string} keywords - The keywords to search for.
-     * @param {Object} [params={}] - Optional parameters for the search.
-     * @return {Promise<Object>} A promise that resolves with the search results.
-     */
-    static async search(keywords, params = {}) {
-        const { data } = await request({
-            method: "GET",
-            url: `/search`,
-            params: {
-                keywords: keywords,
-                params: params
-            }
-        })
+	/**
+	 * Performs a search using the provided keywords and optional parameters.
+	 *
+	 * @description
+	 * This method is used to perform a search using the provided keywords and optional parameters.
+	 * Additionally, supports for external addons to extend the search functionality.
+	 *
+	 * @param {string} keywords - The keywords to search for.
+	 * @param {Object} [params={}] - Optional parameters for the search.
+	 * @param {Array} [returnFields=[]] - An array of fields to return in the results. If empty, all fields will be returned.
+	 * @return {Promise<Object>} A promise that resolves with the search results.
+	 */
+	static async search(keywords, params = {}, returnFields) {
+		let { limit = 50, offset = 0, sort = "desc" } = params
 
-        return data
-    }
+		const { data } = await request({
+			method: "GET",
+			url: `/search`,
+			params: {
+				keywords: keywords,
+				limit: limit,
+				offset: offset,
+				sort: sort,
+				fields: params.fields,
+			},
+		})
 
-    /**
-     * Searches for users with the given keywords and optional parameters.
-     *
-     * @param {string} keywords - The keywords to search for.
-     * @param {Object} [params={}] - Optional parameters for the search.
-     * @return {Promise<Object>} A promise that resolves with the search results.
-     */
-    static async userSearch(keywords, {limit = 50} = {}) {
-        const { data } = await request({
-            method: "GET",
-            url: `/users/search`,
-            params: {
-                keywords: keywords,
-                limit: limit,
-            }
-        })
+		let results = await processAddons({
+			operation: "search",
+			initialData: data,
+			fnArguments: [
+				keywords,
+				{
+					limit: limit,
+					offset: offset,
+					sort: sort,
+				},
+			],
+			normalizeAddonResult: ({ currentData, addonResult }) => {
+				return standartListMerge(currentData, addonResult)
+			},
+		})
 
-        return data
-    }
+		if (Array.isArray(returnFields)) {
+			return Array.from(new Set(returnFields)).reduce((acc, field) => {
+				acc[field] = results[field]
+				return acc
+			}, {})
+		}
 
-    /**
-     * Performs a quick search using the provided parameters.
-     *
-     * @param {Object} params - The parameters for the search.
-     * @return {Promise<Object>} A promise that resolves with the search results data.
-     */
-    static async quickSearch(params) {
-        const response = await request({
-            method: "GET",
-            url: "/search/quick",
-            params: params
-        })
-
-        return response.data
-    }
+		return results
+	}
 }
