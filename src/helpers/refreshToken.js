2f77c50635f6b6448d122a57be25aa4f13202e8e
SrGooglo <srgooglo@ragestudio.net>
2025-03-25 22:53:31 +0000

    support for new ws manager

--- src/helpers/refreshToken.js
+++ src/helpers/refreshToken.js
@@ -1,42 +1,46 @@
 import SessionModel from "../models/session"
-import { reauthenticateWebsockets } from ".."
+
 export default async () => {
-    __comty_shared_state.eventBus.emit("session:refreshing")
-    __comty_shared_state.refreshingToken = true
+	__comty_shared_state.eventBus.emit("session:refreshing")
+	__comty_shared_state.refreshingToken = true
 
-    // send request to regenerate token
-    const response = await __comty_shared_state.baseRequest({
-        method: "POST",
-        url: "/auth",
-        data: {
-            authToken: await SessionModel.token,
-            refreshToken: await SessionModel.refreshToken,
-        }
-    }).catch((error) => {
-        return false
-    })
+	// send request to regenerate token
+	const response = await __comty_shared_state
+		.baseRequest({
+			method: "POST",
+			url: "/auth",
+			data: {
+				authToken: await SessionModel.token,
+				refreshToken: await SessionModel.refreshToken,
+			},
+		})
+		.catch((error) => {
+			return false
+		})
 
-    if (!response) {
-        __comty_shared_state.refreshingToken = false
+	if (!response) {
+		__comty_shared_state.refreshingToken = false
 
-        throw new Error("Failed to regenerate token.")
-    }
+		throw new Error("Failed to regenerate token.")
+	}
 
-    if (!response.data?.token) {
-        __comty_shared_state.refreshingToken = false
+	if (!response.data?.token) {
+		__comty_shared_state.refreshingToken = false
 
-        throw new Error("Failed to regenerate token, invalid server response.")
-    }
+		throw new Error("Failed to regenerate token, invalid server response.")
+	}
 
-    // set new token
-    SessionModel.token = response.data.token
-    SessionModel.refreshToken = response.data.refreshToken
+	// set new token
+	SessionModel.token = response.data.token
+	SessionModel.refreshToken = response.data.refreshToken
 
-    // emit event
-    __comty_shared_state.eventBus.emit("session:refreshed")
-    __comty_shared_state.refreshingToken = false
+	// emit event
+	__comty_shared_state.eventBus.emit("session:refreshed")
+	__comty_shared_state.refreshingToken = false
 
-    reauthenticateWebsockets()
+	if (typeof __comty_shared_state.ws === "object") {
+		await __comty_shared_state.ws.connectAll()
+	}
 
-    return true
+	return true
 }
--- src/index.js
+++ src/index.js
@@ -1,164 +1,24 @@
 import pkg from "../package.json"
 import EventEmitter from "@foxify/events"
-
 import axios from "axios"
-import { io } from "socket.io-client"
 
-import { createHandlers } from "./models"
+import AddonsManager from "./addons"
+import WebsocketManager from "./ws"
 import Storage from "./helpers/withStorage"
-import remote from "./remote"
+import Remotes from "./remotes"
 
-globalThis.isServerMode = typeof window === "undefined" && typeof global !== "undefined"
+globalThis.isServerMode =
+	typeof window === "undefined" && typeof global !== "undefined"
 
 if (globalThis.isServerMode) {
-    const { Buffer } = require("buffer")
+	const { Buffer } = require("buffer")
 
-    globalThis.b64Decode = (data) => {
-        return Buffer.from(data, "base64").toString("utf-8")
-    }
-    globalThis.b64Encode = (data) => {
-        return Buffer.from(data, "utf-8").toString("base64")
-    }
-}
-
-/**
- * Creates websockets by disconnecting and removing listeners from existing instances,
- * then creating new instances for each websocket in the remote.websockets array.
- * Registers event listeners for connection, disconnection, reconnection, error, and any other events.
- *
- * @return {Promise<void>} A promise that resolves when all websockets have been created and event listeners have been registered.
- */
-export async function createWebsockets() {
-    if (!remote.websockets) {
-        return false
-    }
-
-    const instances = globalThis.__comty_shared_state.sockets
-
-    for (let [key, instance] of Object.entries(instances)) {
-        if (instance.connected) {
-            // disconnect first
-            instance.disconnect()
-        }
-
-        // remove current listeners
-        instance.removeAllListeners()
-
-        delete globalThis.__comty_shared_state.sockets[key]
-    }
-
-    for (let ws of remote.websockets) {
-        let opts = {
-            transports: ["websocket"],
-            autoConnect: ws.autoConnect ?? true,
-            forceNew: true,
-            path: ws.path,
-            ...ws.params ?? {},
-        }
-
-        if (ws.noAuth !== true) {
-            opts.auth = {
-                token: Storage.engine.get("token"),
-            }
-        }
-
-        globalThis.__comty_shared_state.sockets[ws.namespace] = io(remote.origin, opts)
-    }
-
-    // regsister events
-    for (let [key, instance] of Object.entries(instances)) {
-        instance.on("connect", () => {
-            //console.debug(`[WS-API][${key}] Connected`)
-
-            globalThis.__comty_shared_state.eventBus.emit(`${key}:connected`)
-        })
-
-        instance.on("disconnect", () => {
-            console.debug(`[WS-API][${key}] Disconnected`)
-
-            globalThis.__comty_shared_state.eventBus.emit(`${key}:disconnected`)
-        })
-
-        instance.on("reconnect", () => {
-            console.debug(`[WS-API][${key}] Reconnected`)
-
-            globalThis.__comty_shared_state.eventBus.emit(`${key}:reconnected`)
-
-            reauthenticateWebsockets()
-        })
-
-        instance.on("error", (error) => {
-            console.error(`[WS-API][${key}] Error`, error)
-
-            globalThis.__comty_shared_state.eventBus.emit(`${key}:error`, error)
-        })
-
-        instance.onAny((event, ...args) => {
-            console.debug(`[WS-API][${key}] Event (${event})`, ...args)
-
-            globalThis.__comty_shared_state.eventBus.emit(`${key}:${event}`, ...args)
-        })
-    }
-}
-
-/**
- * Disconnects all websocket instances by calling the `disconnect` method on each instance.
- *
- * @return {Promise<void>} A promise that resolves when all websocket instances have been disconnected.
- */
-export async function disconnectWebsockets() {
-    const instances = globalThis.__comty_shared_state.sockets
-
-    for (let [key, instance] of Object.entries(instances)) {
-        if (instance.connected) {
-            instance.disconnect()
-        }
-    }
-}
-
-/**
- * Reconnects all websocket instances by disconnecting and reconnecting them with the current token.
- *
- * @return {Promise<void>} A promise that resolves when all websocket instances have been reconnected.
- */
-export async function reconnectWebsockets() {
-    const instances = globalThis.__comty_shared_state.sockets
-
-    for (let [key, instance] of Object.entries(instances)) {
-        if (instance.connected) {
-            // disconnect first
-            instance.disconnect()
-        }
-
-        instance.auth = {
-            token: Storage.engine.get("token"),
-        }
-
-        instance.connect()
-    }
-}
-
-/**
- * Reauthenticates all websocket instances with the current token. If a websocket instance is not connected, it connects to the server. If it is connected, it emits an "auth:reauth" event with the current token.
- *
- * @return {Promise<void>} Promise that resolves when all websocket instances have been reauthenticated.
- */
-export async function reauthenticateWebsockets() {
-    const instances = globalThis.__comty_shared_state.sockets
-
-    for (let [key, instance] of Object.entries(instances)) {
-        const token = Storage.engine.get("token")
-
-        instance.auth = {
-            token: token,
-        }
-
-        if (!instance.connected) {
-            instance.connect()
-        } else {
-            instance.emit("auth:reauth", token)
-        }
-    }
+	globalThis.b64Decode = (data) => {
+		return Buffer.from(data, "base64").toString("utf-8")
+	}
+	globalThis.b64Encode = (data) => {
+		return Buffer.from(data, "utf-8").toString("base64")
+	}
 }
 
 /**
@@ -168,54 +28,56 @@ export async function reauthenticateWebsockets() {
  * @return {Object} sharedState - Object containing eventBus, mainOrigin, baseRequest, sockets, rest, and version
  */
 export function createClient({
-    accessKey = null,
-    privateKey = null,
-    enableWs = false,
-    origin = remote.origin,
+	accessKey = null,
+	privateKey = null,
+	enableWs = false,
+	origin = Remotes.origin,
+	eventBus = new EventEmitter(),
 } = {}) {
-    const sharedState = globalThis.__comty_shared_state = {
-        eventBus: new EventEmitter(),
-        mainOrigin: origin,
-        baseRequest: null,
-        sockets: new Map(),
-        rest: null,
-        version: pkg.version,
-    }
+	const sharedState = (globalThis.__comty_shared_state = {
+		eventBus: eventBus,
+		mainOrigin: origin,
+		baseRequest: null,
+		ws: null,
+		rest: null,
+		version: pkg.version,
+		addons: new AddonsManager(),
+	})
 
-    sharedState.rest = createHandlers()
+	if (privateKey && accessKey && globalThis.isServerMode) {
+		Storage.engine.set("token", `${accessKey}:${privateKey}`)
+	}
 
-    if (privateKey && accessKey && globalThis.isServerMode) {
-        Storage.engine.set("token", `${accessKey}:${privateKey}`)
-    }
+	sharedState.baseRequest = axios.create({
+		baseURL: origin,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
 
-    sharedState.baseRequest = axios.create({
-        baseURL: origin,
-        headers: {
-            "Content-Type": "application/json",
-        }
-    })
+	// create a interceptor to attach the token every request
+	sharedState.baseRequest.interceptors.request.use((config) => {
+		// check if current request has no Authorization header, if so, attach the token
+		if (!config.headers["Authorization"]) {
+			const sessionToken = Storage.engine.get("token")
 
-    // create a interceptor to attach the token every request
-    sharedState.baseRequest.interceptors.request.use((config) => {
-        // check if current request has no Authorization header, if so, attach the token
-        if (!config.headers["Authorization"]) {
-            const sessionToken = Storage.engine.get("token")
+			if (sessionToken) {
+				config.headers["Authorization"] =
+					`${globalThis.isServerMode ? "Server" : "Bearer"} ${sessionToken}`
+			} else {
+				console.warn("Making a request with no session token")
+			}
+		}
 
-            if (sessionToken) {
-                config.headers["Authorization"] = `${globalThis.isServerMode ? "Server" : "Bearer"} ${sessionToken}`
-            } else {
-                console.warn("Making a request with no session token")
-            }
-        }
+		return config
+	})
 
-        return config
-    })
+	if (enableWs == true) {
+		__comty_shared_state.ws = new WebsocketManager()
+		sharedState.ws.connectAll()
+	}
 
-    if (enableWs) {
-        createWebsockets()
-    }
-
-    return sharedState
+	return sharedState
 }
 
 export default createClient
