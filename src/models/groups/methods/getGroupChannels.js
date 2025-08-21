d194f7833ffa55365ab1f727899610147fcd86ed
srgooglo <srgooglo@ragestudio.net>
2025-08-22 00:40:31 +0200

    Add methods to fetch group channels and a single channel

---
+++ src/models/groups/methods/getGroupChannels.js
@@ -0,0 +1,18 @@
+import request from "../../../request"
+
+export default async (group_id, { limit = 50, offset = null } = {}) => {
+	if (typeof group_id !== "string") {
+		throw new Error("group_id is required")
+	}
+
+	const response = await request({
+		method: "GET",
+		url: `/groups/${group_id}/channels`,
+		params: {
+			limit: limit,
+			offset: offset,
+		},
+	})
+
+	return response.data
+}
