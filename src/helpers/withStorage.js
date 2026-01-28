1821cee08379bac91ddf0cbd60f589db328053f4
srgooglo <srgooglo@ragestudio.net>
2026-01-28 21:52:00 +0100

    format

--- src/helpers/withStorage.js
+++ src/helpers/withStorage.js
@@ -1,31 +1,32 @@
 import jscookies from "js-cookie"
 
 class InternalStorage {
-    #storage = {}
+	#storage = {}
 
-    get(key) {
-        // get value from storage
-        return this.#storage[key]
-    }
+	get(key) {
+		// get value from storage
+		return this.#storage[key]
+	}
 
-    set(key, value) {
-        // storage securely in memory
-        return this.#storage[key] = value
-    }
+	set(key, value) {
+		// storage securely in memory
+		return (this.#storage[key] = value)
+	}
 }
 
 export default class Storage {
-    static get engine() {
-        // check if is running in browser, if is import js-cookie
-        // else use in-memory safe storage
-        if (typeof window !== "undefined") {
-            return jscookies
-        }
+	static get engine() {
+		// check if is running in browser, if is import js-cookie
+		// else use in-memory safe storage
+		if (typeof window !== "undefined") {
+			return jscookies
+		}
 
-        if (!globalThis.__comty_shared_state["_internal_storage"]) {
-            globalThis.__comty_shared_state["_internal_storage"] = new InternalStorage()
-        }
+		if (!globalThis.__comty_shared_state["_internal_storage"]) {
+			globalThis.__comty_shared_state["_internal_storage"] =
+				new InternalStorage()
+		}
 
-        return globalThis.__comty_shared_state["_internal_storage"]
-    }
+		return globalThis.__comty_shared_state["_internal_storage"]
+	}
 }
