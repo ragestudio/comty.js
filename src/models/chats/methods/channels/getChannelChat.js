eb1008f9a6b2a554c6d15b5db79596d2ad217ee4
srgooglo <srgooglo@ragestudio.net>
2026-01-28 21:55:35 +0100

    Add channel chat methods for get, send, and delete

---
+++ src/models/chats/methods/channels/getChannelChat.js
@@ -0,0 +1,19 @@
+import request from "../../../../request"
+
+export default async (group_id, channel_id, params) => {
+	if (typeof group_id !== "string") {
+		throw new Error("group_id must be a string")
+	}
+
+	if (typeof channel_id !== "string") {
+		throw new Error("channel_id must be a string")
+	}
+
+	const response = await request({
+		method: "GET",
+		url: `/chats/channels/${group_id}/${channel_id}`,
+		params: params,
+	})
+
+	return response.data
+}
