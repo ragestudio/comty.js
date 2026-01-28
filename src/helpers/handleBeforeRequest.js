f02c503bf506eff0180e2861224808e4157e25bb
srgooglo <srgooglo@ragestudio.net>
2026-01-28 21:51:14 +0100

    format

--- src/helpers/handleBeforeRequest.js
+++ src/helpers/handleBeforeRequest.js
@@ -1,7 +1,8 @@
 export default async () => {
-    if (__comty_shared_state.refreshingToken === true) {
-        await new Promise((resolve) => {
-            __comty_shared_state.eventBus.once("session:refreshed", resolve)
-        })
-    }
+	// await to token being refreshed
+	if (__comty_shared_state.refreshingToken === true) {
+		await new Promise((resolve) => {
+			__comty_shared_state.eventBus.once("session:refreshed", resolve)
+		})
+	}
 }
