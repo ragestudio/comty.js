6bbcddffb162a3884280cfbad923fe20e5019e95
srgooglo <srgooglo@ragestudio.net>
2026-07-07 19:53:21 +0200

    Add getGroupConnections method to GroupMembers class

--- src/models/groups/members/index.js
+++ src/models/groups/members/index.js
@@ -1,9 +1,11 @@
 import getGroupMembersMethod from "./methods/getGroupMembers"
 import getGroupMemberMethod from "./methods/getGroupMember"
 import removeGroupMemberMethod from "./methods/removeGroupMember"
+import getGroupConnectionsMethod from "./methods/getGroupMembersConnections"
 
 export default class GroupMembers {
 	static list = getGroupMembersMethod
 	static get = getGroupMemberMethod
 	static remove = removeGroupMemberMethod
+	static connections = getGroupConnectionsMethod
 }
